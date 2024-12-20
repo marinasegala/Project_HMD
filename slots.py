4e6e93c14fc64bcecec2beac7cd00ed20afdec75
marinasegala <95535623+marinasegala@users.noreply.github.com>
2024-12-20 17:04:40 +0100

    Update slots.py

--- slots.py
+++ slots.py
@@ -8,16 +8,29 @@ class Ordering():
         self.pagament = None
         self.price = None
 
-        self.possible_name = True # True if the values are all in the csv file 
-        self.possible_qty = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20]
-        self.possible_address = [] # don't care
-        self.possible_phone = [] # don't care
-        self.possible_gift = [True, False] 
-        self.possible_pagament = ['cash', 'credit card', 'paypal']
-        self.possible_price = True # True if the values are all in the csv file
+        self.possibilities = {
+            "name": True,
+            "qty": [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20],
+            "address": True,
+            "phone": True,
+            "gift": [True, False],
+            "pagament": ['cash', 'credit card', 'paypal'],
+            "price": True
+        }
 
     def __str__(self):
         return f"Name: {self.name}, Quantity: {self.qty}, Address: {self.address}, Phone: {self.phone}, Gift: {self.gift}, Pagament: {self.pagament}, Price: {self.price}"
+
+    def extract(self):
+        return {
+            "name": self.name,
+            "qty": self.qty,
+            "address": self.address,
+            "phone": self.phone,
+            "gift": self.gift,
+            "pagament": self.pagament,
+            "price": self.price
+        }
 
 
 class ParingFood():
@@ -29,16 +42,28 @@ class ParingFood():
         self.grape = None
         self.style = None
 
-        self.possible_kind = ['Porsecco', 'Pinot', 'Gavi', 'Valdobbiadene', 'Macon', 'Saumur', 'Champagne', 'Ripasso', 'Brunello di Montalcino', 'Etna', 'Bordeaux Supérieur', 'Haut-Médoc', 'Rully', 'Fleurie', 'Saint-Émilion', 'Cotes Du Rhone', 'Lirac', 'La Clape', 'Lugana','Cotes De Provence', 'Coteaux Varois-En-Provence', "Coteaux D'Aix-En-Provence"]
-        self.possible_food_pairing = ['aperitif', 'starters', 'first course', 'cold cuts', 'savory pies', 'pheasant meat', 'pumpkin dishes', 'meat', 'fish', 'white meat', 'red meat', 'poultry', 'pork', 'game', 'mashroom', 'cheese', 'dessert']
-        self.possible_year = True # True if the values are all in the csv file
-        self.possible_name = True # True if the values are all in the csv file
-        self.possible_grape = True # True if the values are all in the csv file
-        self.possible_style = True # True if the values are all in the csv file
+        self.possibilities = {
+            "kind": ['Porsecco', 'Pinot', 'Gavi', 'Valdobbiadene', 'Macon', 'Saumur', 'Champagne', 'Ripasso', 'Brunello di Montalcino', 'Etna', 'Bordeaux Supérieur', 'Haut-Médoc', 'Rully', 'Fleurie', 'Saint-Émilion', 'Cotes Du Rhone', 'Lirac', 'La Clape', 'Lugana','Cotes De Provence', 'Coteaux Varois-En-Provence', "Coteaux D'Aix-En-Provence"],
+            "food_pairing": ['aperitif', 'starters', 'first course', 'cold cuts', 'savory pies', 'pheasant meat', 'pumpkin dishes', 'meat', 'fish', 'white meat', 'red meat', 'poultry', 'pork', 'game', 'mashroom', 'cheese', 'dessert'],
+            "year": True,
+            "name": True,
+            "grape": True,
+            "style": True
+            
+        }
 
     def __str__(self):
         return f"Kind: {self.kind}, Food Pairing: {self.food_pairing}, Year: {self.year}, Name: {self.name}, Grape: {self.grape}, Style: {self.style}"
 
+    def extract(self):
+        return {
+            "kind": self.kind,
+            "food_pairing": self.food_pairing,
+            "year": self.year,
+            "name": self.name,
+            "grape": self.grape,
+            "style": self.style
+        }
 
 class AskInfo():
     def __init__(self):
@@ -54,17 +79,42 @@ class AskInfo():
         self.style = None
         self.food_pairing = None
 
-        self.possible_name = True # True if the values are all in the csv file
-        self.possible_kind = True # True if the values are all in the csv file
-        self.possible_country = ['Italy', 'France']
-        self.possible_region = True # True if the values are all in the csv file
-        self.possible_color = ['white', 'red', 'rose']
-        self.possible_grape = True # True if the values are all in the csv file
-        self.possible_abv = True # True if the values are all in the csv file
-        self.possible_closure = ['Natural Cork', 'Screwcap', 'Synthetic Cork'] 
-        self.possible_flavor = True # True if the values are all in the csv file
-        self.possible_style = True # True if the values are all in the csv file
-        self.possible_foodpairing = ['aperitif', 'starters', 'first course', 'cold cuts', 'savory pies', 'pheasant meat', 'pumpkin dishes', 'meat', 'fish', 'white meat', 'red meat', 'poultry', 'pork', 'game', 'mashroom', 'cheese', 'dessert']
-
+        self.possibilities = {
+            'name': True,
+            'kind': True,
+            'country': ['Italy', 'France'],
+            'region': True,
+            'color': ['white', 'red', 'rose'],
+            'grape': True,
+            'abv': True,
+            'closure': ['Natural Cork', 'Screwcap', 'Synthetic Cork'],
+            'flavor': True,
+            'style': True,
+            'foodpairing': ['aperitif', 'starters', 'first course', 'cold cuts', 'savory pies', 'pheasant meat', 'pumpkin dishes', 'meat', 'fish', 'white meat', 'red meat', 'poultry', 'pork', 'game', 'mashroom', 'cheese', 'dessert']
+        }
+        
     def __str__(self):
         return f"Name: {self.name}, Kind: {self.kind}, Country: {self.country}, Region: {self.region}, Color: {self.color}, Grape: {self.grape}, ABV: {self.abv}, Closure: {self.closure}, Flavor: {self.flavor}, Style: {self.style}, Food Pairing: {self.food_pairing}"
+    
+    def extract(self):
+        return {
+            "name": self.name,
+            "kind": self.kind,
+            "country": self.country,
+            "region": self.region,
+            "color": self.color,
+            "grape": self.grape,
+            "abv": self.abv,
+            "closure": self.closure,
+            "flavor": self.flavor,
+            "style": self.style,
+            "food_pairing": self.food_pairing
+        }
+    
+
+def assign_field (intent: object, field: str, value: str):
+    # find the correct possible field
+    for possible in intent.possibilities:
+        if field in possible:
+            setattr(intent, field, value)
+            break
