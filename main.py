28d325dffc68f3d6706a5b538f23fb6b51151aa2
marinasegala <95535623+marinasegala@users.noreply.github.com>
2024-12-22 17:43:09 +0100

    Update main.py

--- main.py
+++ main.py
@@ -4,10 +4,14 @@ import re
 import torch
 import json
 import os
+import logging
 from utils import PROMPTS
 import requests
 from typing import Union
 from slots import Ordering, ParingFood, AskInfo, assign_field
+
+logger = logging.getLogger('Dialogue')
+logger.setLevel(logging.DEBUG)
 
 def generate_response_Ollama(prompt, model="llama3.1:70b"):
     
@@ -56,43 +60,20 @@ def parsing_json(text):
 #             if key in self.slots.__dict__:
 #                 self.slots.__dict__[key] = nlu_json["slots"][key]
 
-class RisottoSlots:
-    def __init__(self):
-        self.slot = {
-            "type": None,
-            "cheese_on_top": None,
-            "num_slice_meat": None,
-            "size": None,
-            "consistency": None
-        }
-
-        self.values = {
-            "type": ["funghi", "speck", "milanese"],
-            "cheese_on_top": ["grana", "parmigiano", "senza lattosio", "no"],
-            "num_slice_meat": ["0", "1", "2", "3"],
-            "size": ["baby", "normal", "big"],
-            "consistency": ["creamy", "normal"]
-        }
-
-    def items(self):
-        return self.slot.items()
-    
-class DrinkSlots:
-    def __init__(self):
-        self.type = None
-
 def extract_action_and_argument(input_string):
     ## TODO add check in case there is more in the output string from the LLM
     # Remove any ' characters from the input string
     input_string = input_string.replace("'", "")
     input_string = input_string.replace("\"", "")
     # Define the regex pattern for extracting action and argument
-    pattern = r'(\w+)\((\w+)\)'
+    pattern = r'(\w+)\(([\w\s=]+)\)' #r'(\w+)\((.*)\)\s*=\s*(.*)' #r'(\w+)\((\w+)\)'
     match = re.match(pattern, input_string)
     
     if match:
         action = match.group(1)  # Extract the action
         argument = match.group(2)  # Extract the argument
+        if '=' in argument:
+            argument = argument.split('=')[1]
         return action, argument
 
 class History():
@@ -116,11 +97,11 @@ class History():
     def intent_history(self):
         return ', \n'.join(self.intent)
 
-    # def to_msg_history(self)->list[dict]:
-    #     history = [{'role': role, 'content': msg} for role, msg in zip(self.roles, self.messages)]
-    #     if len(history) > 5:
-    #         return history[-5:]
-        # return history
+    def to_msg_history(self)->list[dict]:
+        history = [{'role': role, 'content': msg} for role, msg in zip(self.roles, self.messages)]
+        if len(history) > 5:
+            return history[-5:]
+        return history
 
 class DMTracker:
     def __init__(self):
@@ -158,17 +139,6 @@ class DMTracker:
                     assign_field(self.asking_info, field, input[field])
 
         return intent
-
-        # slots = [x["slots"] for x in self.intentions if x["intent"] == input["intent"]][0]
-
-        # for key, value in slots.items():
-        #     if value == None and input["slots"][key] != 'null':
-        #         slots.slot[key] = input["slots"][key]
-
-        # # reassign the slots to the intent
-        # for x in self.intentions:
-        #     if x["intent"] == input["intent"]:
-        #         x["slots"] = slots
     
     def dictionary(self, intent_ret):
         for x in self.intentions:
@@ -191,18 +161,58 @@ class DMTracker:
                 print(f"Dict: {dict_ret}")
                 return dict_ret
 
+class Analizer():
+    # used for pre-processing the input text - find all the intent possible in the text
+    def __init__(self, history):
+        self.history = history
+        pass
+    
+    def __call__(self, text):
+        # extract all the intent in the text
+        # last_int = self.history.to_msg_history()
+        # last_int = last_int[-5:] if (len(last_int) > 5) else last_int
+        # last_int = "\n".join([f"{k['role']}: {k['content']}"  for k in last_int])
+        # logger.info(f"History: {last_int}")
+        
+        nlu_text = PROMPTS["PRE-NLU"] + '\n' + text
+
+        intents = generate_response_Ollama(nlu_text)
+        print(f"Intents: {intents}")
+        
+
 class NLU():
-    def __init__(self):
+    def __init__(self, history):
+        self.history = history
+        self.analizer = Analizer(history)
         pass
     
     def __call__(self, user_input):
-        nlu_text = PROMPTS["NLU"] + user_input
+        list_intents = self.analizer(user_input) #TODO 
+
+        match list_intents[0]:
+            case "wine_ordering":
+                prompt = PROMPTS["Infos"]
+            case "paring_food":
+                prompt = PROMPTS["Food"]
+            case "asking_info":
+                prompt = PROMPTS["Order"]
+        
+        prompt = prompt + '\n' + PROMPTS["NLU"]
+
+        last_int = self.history.to_msg_history()
+        last_int = last_int[-5:] if (len(last_int) > 5) else last_int
+        last_int = "\n".join([f"{k['role']}: {k['content']}"  for k in last_int])
+        logger.info(f"History: {last_int}")
+
+        nlu_text = prompt + '\n' + last_int + '\n' + user_input
+
         nlu_output = generate_response_Ollama(nlu_text)
         nlu_output = nlu_output.strip()
 
         with open("nlu_output.txt", "w") as file:
             file.write(nlu_output)
         nlu_js = parsing_json(nlu_output)
+        logger.info(f"NLU output: {nlu_js}")
         return nlu_js
 
 class DM():
@@ -219,21 +229,30 @@ class DM():
         with open("dm_output.txt", "w") as file:
             file.write(dm_output)
         
-        print(f"DM: {dm_output}")
+        logger.debug(f"DM output: {dm_output}")
         action, argument = extract_action_and_argument(dm_output)
         return action, argument
     
+class NLG():
+    def __init__(self):
+        pass
+
+    def __call__(self, action, argument):
+        nlg_text = PROMPTS["NLG"] + '\n' + f"{action}({argument})"
+        nlg_output = generate_response_Ollama(nlg_text)
+        nlg_output = nlg_output.strip()
+        return nlg_output
 
 class Dialogue:
     def __init__(self):
-        self.nlu = NLU()
-        self.dm = DM()
-        self.nlg = None
         self.tracker = DMTracker()
         self.history = History()
+        self.nlu = NLU(self.history)
+        self.dm = DM()
+        self.nlg = NLG()
 
     def start(self):
-        starting = 'Hi, I am your wine assistant. How can I help you?'
+        starting = PROMPTS["START"]
         self.history.add_msg(starting, 'assistant', 'init')
         user_input = input(starting + '\n')
         self.history.add_msg(user_input, 'user', 'input')
@@ -245,31 +264,25 @@ class Dialogue:
             infos = self.nlu(user_input)
             print(f"NLU: {infos}")
             intent = self.tracker.update(infos)
-            #
+            logger.info(self.tracker)
             # get the DM output
             action, arg = self.dm(self.tracker, intent)
-            print(f"Action: {action}, Argument: {arg}")
-            # nlu_output = dm_tracker.return_values(nlu_js["intent"])
+            logger.info(f'Action: {action}, Argument: {arg}')
 
-            # # get the DM output
-            # dm_text = PROMPTS["DM"] +'\n'+ nlu_output
-            # dm_output = query_ollama(dm_text)
-            # print(f"DM: {dm_output}")
-
-            # # Optional Pre-Processing for NLG
-            # dm_output = dm_output.strip()
-
-            # # get the NLG output
-            # nlg_text = PROMPTS["NLG"] + dm_output
+            # get the NLG output
+            nlg_output = self.nlg(action, arg)
+            self.history.add_msg(nlg_output, 'assistant', action)
             
-            # nlg_output = query_ollama(nlg_text)
-
-            # print(f"NLG: {nlg_output}")
+            user_input = input(nlg_output + '\n')
+            self.history.add_msg(user_input, 'user', 'input')
 
 def main():
     # args = get_args()
+    logging.basicConfig(filename="app.log", encoding="utf-8", filemode="a")
+    logger.info("Starting the dialogue")
     dg = Dialogue()
     dg.start()
+    print(dg.history.to_msg_history())
 
 if __name__ == "__main__":
     main()
