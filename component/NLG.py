2faa1718570c8d1989e3de47b4d382e5a3be6a70
marinasegala <95535623+marinasegala@users.noreply.github.com>
2024-12-30 11:30:43 +0100

    Update NLG.py

--- component/NLG.py
+++ component/NLG.py
@@ -1,13 +1,24 @@
-from utils import PROMPTS, generate_response_Ollama
-
+from utils import PROMPTS, generate_response_Ollama, generate
 
 class NLG():
-    def __init__(self):
+    def __init__(self, history, model, tokenizer, args):
+        self.history = history
+        self.model = model
+        self.tokenizer = tokenizer
+        self.args = args
         pass
 
     def __call__(self, action, argument):
-        nlg_text = PROMPTS["NLG"] + '\n' + f"{action}({argument})"
-        print(f"NLG: {nlg_text}")
-        nlg_output = generate_response_Ollama(nlg_text)
+        last_int = self.history.to_msg_history()
+        last_int = last_int[-5:] if (len(last_int) > 5) else last_int
+        last_int = "\n".join([f"{k['role']}: {k['content']}"  for k in last_int])
+        
+        nlg_text =  f"{action}({argument})\n" + last_int 
+
+        nlg_text = self.args.chat_template.format(PROMPTS["PRE-NLG"], nlg_text)
+        nlg_input = self.tokenizer(nlg_text, return_tensors="pt").to(self.model.device)
+        nlg_output = generate(self.model, nlg_input, self.tokenizer, self.args)
+        # print(f"NLG: {nlg_text}")
+        # nlg_output = generate_response_Ollama(nlg_text)
         nlg_output = nlg_output.strip()
         return nlg_output
