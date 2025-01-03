bef4e4b7543340938b36168c0c29c9d283d43164
marinasegala <95535623+marinasegala@users.noreply.github.com>
2025-01-03 11:27:20 +0100

    Update extractor_data.py

--- extractor_csv.py
+++ extractor_csv.py
@@ -1,5 +1,34 @@
-import pandas as pd
+# import pandas as pd
+import json
 
+import json
+
+def extract_db_init(field):
+    with open('data.json', 'r') as file:
+        data = json.load(file)
+    
+    #capitalize each word
+    field = field.capitalize() 
+    list_values = list(set(item[field] for item in data if field in item))
+    list_values = [x for x in list_values if str(x) != 'nan' and x != '']
+    new_list = []
+    for x in list_values:
+        if ',' in x:
+            adding = x.split(', ')
+            for i in adding:
+                if i not in new_list:
+                    new_list.append(i)
+        elif x not in new_list:
+            new_list.append(x)
+
+    # separate the values of the same element in the list
+    #remove nan values from list
+
+    if type(new_list[0]) == str:
+        new_list = [x.lower() for x in new_list]
+
+    return new_list + ['unknown']
+'''
 def extract_db_init(field):
     data = pd.read_csv('chatbots/dataset2.csv')
     #capitalize each word
@@ -25,30 +54,6 @@ def extract_db_init(field):
 
     return new_list + ['unknown']
 
-# def extract_database(field, value):
-#     data = pd.read_csv('dataset2.csv')
-#     # first letter to uppercase
-#     field = field.capitalize()
-#     list_values = data[field].unique()
-#     #remove nan values from list
-#     list_values = [x for x in list_values if str(x) != 'nan']
-
-#     if type(list_values[0]) == str:
-#         print(list_values[0])
-#         list_values = [x.lower() for x in list_values]
-#         print(list_values[0])
-    
-#     # print(list_values)
-#     if type(value) == str:
-#         value = value.lower()
-
-#     # return the value if it is an element of the list
-#     for x in list_values:
-#         if x == value:
-#             return x
-
-#     return None
-
 def searching_wine(tracker, intent):
     dict_info = tracker.dictionary(intent)
     slots = dict_info["slots"]
@@ -58,15 +63,40 @@ def searching_wine(tracker, intent):
     # filter the data according to the slots
     list_wine = [x for x in range(1, len(data)+1)]
 
-
     for index, row in data.iterrows():
         for slot in slots:
+            field = slot.split('_')[0] if '_' in slot else slot
             if slots[slot] != None:
-                if str(row[slot.capitalize()]) == 'nan':
+                if str(row[field.capitalize()]) == 'nan':
                     list_wine.remove(index+1)
                     continue
                 search = slots[slot].capitalize() if type(slots[slot]) == str else slots[slot]
-                if search not in row[slot.capitalize()]:
+                if search not in row[field.capitalize()]:
+                    list_wine.remove(index+1)
+
+    return list_wine
+'''
+import json
+
+def searching_wine(tracker, intent):
+    dict_info = tracker.dictionary(intent)
+    slots = dict_info["slots"]
+
+    with open('data.json', 'r') as file:
+        data = json.load(file)
+    
+    # filter the data according to the slots
+    list_wine = [x for x in range(1, len(data)+1)]
+
+    for index, item in enumerate(data):
+        for slot in slots:
+            field = slot.split('_')[0] if '_' in slot else slot
+            if slots[slot] is not None:
+                if str(item.get(field.capitalize(), 'nan')) == 'nan':
+                    list_wine.remove(index+1)
+                    continue
+                search = slots[slot].capitalize() if isinstance(slots[slot], str) else slots[slot]
+                if search not in item[field.capitalize()]:
                     list_wine.remove(index+1)
 
     return list_wine
